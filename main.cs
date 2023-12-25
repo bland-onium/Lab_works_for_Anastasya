aaceb78ef23bb45311f3a679b0f49861d71b619e
bland_onium <107197514+bland-onium@users.noreply.github.com>
2023-12-26 02:13:53 +0500

    Add files via upload
    An one little parser which scan sites on having phone numbers and adresses (Only for Russia)

--- main.cs
+++ main.cs
@@ -1,14 +1,23 @@
-using System;
-using System.ComponentModel.Design;
-using System.Linq.Expressions;
-using System.Runtime.InteropServices;
+﻿/*                  Вариант 8
+ * Разработать класс по анализу HTML-содержимого web-страниц заданного интернет ресурса
+ * Цель поиска – контактная информация (адрес, телефон)*/
 
-namespace Nastya {
-    class Program
+using Lab_4_Nastya;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ast
+{
+    internal class main
     {
         static void Main()
         {
-            Menu.StartProgram();
+            Menu.menu();
         }
     }
 }
